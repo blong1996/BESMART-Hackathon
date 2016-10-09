8d46f13b9ce8f4f2f4597a1d8e0e78fd34752b90
Brandon Long <blong1996@gmail.com>
2016-10-09 10:50:08 -0700

    Add Assertive to index

---
+++ LetsGoBlack/www/js/controllers.js
@@ -0,0 +1,19 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope) {
+})
+
+.controller('ProfileCtrl', function($scope) {
+})
+
+.controller('SelectTripCtrl', function($scope) {
+ // $scope.friends = Friends.all();
+})
+
+.controller('TripDetailsCtrl', function($scope) {
+//  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+
+.controller('AccountCtrl', function($scope) {
+});
